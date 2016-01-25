4e936b5c8765e80576f949a597273749c732023c
Nikita <nik.europe@gmail.com>
2016-01-25 12:07:43 +0100

    Create Blog1.php

---
+++ Blog1.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01//FR">
+<meta charset="UTF-8">
+
+<body>
+
+<?php
+
+echo "<br />";
+echo "<br />";
+
+echo "Hello World!";
+echo "<br />";
+
+echo 'One line simple string.<br />';
+?>
+
+</body>
+</html>
