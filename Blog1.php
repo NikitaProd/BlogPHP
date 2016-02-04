0542cc2a6248498409fbf7220197f70eb14ec417
Nikita <nik.europe@gmail.com>
2016-02-04 11:20:29 +0100

    Update Blog1.php

--- Blog1.php
+++ Blog1.php
@@ -1,10 +1,10 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01//FR">
 <meta charset="UTF-8">
 
+
 <body>
 
 <?php
-
 echo "<br />";
 echo "<br />";
 
@@ -13,6 +13,61 @@ echo "<br />";
 
 echo 'One line simple string.<br />';
 ?>
+
+<?php
+// This is a single-line comment
+
+# This is also a single-line comment
+
+/*
+This is a multiple-lines comment block
+that spans over multiple
+lines
+*/
+
+
+// You can also use comments to leave out parts of a code line
+$x = 5 /* + 15 */ + 5;
+echo $x;
+
+echo "<br />";
+echo "<br />";
+
+echo "Hello World!";
+echo "<br />";
+
+echo 'One line simple string.<br />';
+echo 'Two line simple string example<br />';
+echo 'Tomorrow I \'ll learn PHP global variables.<br />';
+echo 'This is a bad command : del c:\\*.* <br />';
+
+
+echo "<p> <font color=blue>One line simple string in
+blue color</font> </p>";
+echo "<p> <font color=red>One line simple string in red
+color</font> </p>";
+echo "<p> <font color=green> One line simple string in
+green color</font> </p>";
+
+
+
+$a=1000;
+$b=1200;
+$c=1400;
+echo "<table border=1 cellspacing=0 cellpading=0>
+<tr> <td><font color=blue>Salary of Mr. A is</td> <td>$a$</font></td></tr>
+<tr> <td><font color=blue>Salary of Mr. B is</td> <td>$b$</font></td></tr>
+<tr> <td><font color=blue>Salary of Mr. C is</td> <td>$c$</font></td></tr>
+</table>";
+
+echo '<br />';
+
+echo "<a href=echo-order-list.php>
+Click here to see the current month salary
+statement.</a>";
+
+?>
+
 
 </body>
 </html>
