0fb89e29c97d6c1e3a216befbb17e8a40dfe3109
Nikita <nik.europe@gmail.com>
2016-02-08 16:50:31 +0100

    1

---
+++ Blog.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01//FR">
+<meta charset="UTF-8">
+
+
+<body>
+<h1>Mini Blog</h1>
+
+
+
+<form acion="" method="post">
+<input type="text" name="pseudo" />
+<input type="text" name="message" />
+<input type="submit" value="Entrer"/>
+</form>
+
+<?php
+
+?>
+
+</body>
+</html>
