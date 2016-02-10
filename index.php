e883efdd7b03a5078c4bc0c6b06919690e358075
Nikita <nik.europe@gmail.com>
2016-02-10 16:22:14 +0100

    Update and rename blog.php to index.php

---
+++ index.php
@@ -0,0 +1,12 @@
+
+
+<?php
+include('config.php');
+
+// mysql_connect('localhost', 'root', 'root')*
+// mysql_select_db('blog1');
+
+// phpmyadmin:  table: 1.Articles (id, titre, auteur, contenu)  et
+//                     2.commentaire (id, auteur, contenu, id_article )
+?>
+
