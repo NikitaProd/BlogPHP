95beaf44b037b2a2c1d5b927ed69c5216af35562
Nikita <nik.europe@gmail.com>
2016-02-11 11:51:10 +0100

    Update post.php

--- post.php
+++ post.php
@@ -2,4 +2,25 @@
 <!-- POST PAGE -->
 <?php
 include('config.php');
-?>
+if(!isset($_GET['id']))
+{
+header('Location: index.php');
+}
+else
+{
+$id_get = intval($_GET['id']);
+}
+
+
+$v1 = mysql_query('SELECT * FROM articles WHERE id = "'.$id_get.'"');
+$info_article = mysql_fetch_array($v1);
+if(isset($info_article['id'])) { ?>
+<h1><?php echo htmlspecialchars($info_article['titre']); ?></h1>
+<h5>par <?php echo $info_article['auteur']; ?></h5>
+<br />
+<?php echo nl2br(htmlspecialchars($info_article['contenu']));?>
+<br />
+<br />
+<hr />
+<br />
+
