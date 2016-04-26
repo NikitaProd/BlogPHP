dd40c94b13d936118f0065e1e7312d3c288d7b9f
Nikita <nik.europe@gmail.com>
2016-04-26 18:05:39 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,14 +1,12 @@
 
 
 <?php
-
 // Afficher les erreurs à l'écran
 ini_set('display_errors', 1);
 // Enregistrer les erreurs dans un fichier de log
 ini_set('log_errors', 1);
 // Nom du fichier qui enregistre les logs (attention aux droits à l'écriture)
 ini_set('error_log', dirname(__file__) . '/log_error_php.txt');
-
 ?>
 
 <!-- Blog basique  -->
@@ -38,5 +36,6 @@ Nouveaute N <?php echo $info_article['id']; ?> publie par
 <?php
 }
 ?>
+
 
 <!-- Blog basique THE END -->
